dfe21399c70dd1974db3a2f3bfd53e9866fd9dac
Alivk <alirezavalikhani1998@gmail.com>
2025-12-08 10:46:17 +0330

    feat: Add FileHandler to load books from file and log actions and add input file to read books from it.

---
+++ src/librarymanagementsystem/io/FileHandler.java
@@ -0,0 +1,75 @@
+package librarymanagementsystem.io;
+
+import librarymanagementsystem.enums.BookState;
+import librarymanagementsystem.exception.LogWriteException;
+import librarymanagementsystem.exception.MissingParametersException;
+import librarymanagementsystem.model.Book;
+import librarymanagementsystem.model.Library;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FileHandler {
+
+    private static final String INPUT_FILE = "books_input.txt";
+    private static final String OUTPUT_FILE = "log_output.txt";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public int loadBooksFromFile(Library library) {
+
+        int count = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.trim().isEmpty()) continue;
+
+                String[] bookDetails = line.split(",");
+                if (bookDetails.length != 4) {
+                    logAction("Error", "This line has more or less than 4 columns : " + line);
+                    continue;
+                }
+
+                try {
+                    String title = bookDetails[0].trim();
+                    String author = bookDetails[1].trim();
+                    int yearOfPublication = Integer.parseInt(bookDetails[2].trim());
+                    int bookStateValue = Integer.parseInt(bookDetails[3].trim());
+                    BookState bookState = BookState.fromValue(bookStateValue);
+
+                    Book book = new Book(title, author, yearOfPublication, bookState);
+                    library.addBook(book);
+                    count++;
+                } catch (NumberFormatException e) {
+                    logAction("Error", "Invalid year of publication : " + line);
+                    throw new MissingParametersException("Invalid year of publication : " + line);
+                }
+
+            }
+
+        } catch (FileNotFoundException e) {
+            logAction("Load error", "File not found : " + INPUT_FILE);
+            throw new librarymanagementsystem.exception.FileNotFoundException("File not found");
+        } catch (IOException e) {
+            logAction("Error", "IO error : " + e.getMessage());
+            throw new librarymanagementsystem.exception.IOException("Error loading file : " + e.getMessage());
+        }
+
+        return count;
+    }
+
+    public void logAction(String operation, String result) {
+        String timestamp = LocalDateTime.now().format(FORMATTER);
+        String log = String.format("[%s] Operation : %s | Result : %s", timestamp, operation, result);
+
+        try {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(OUTPUT_FILE, true))) {
+                bw.write(log);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            throw new LogWriteException("Error in writing output file!");
+        }
+    }
+}
