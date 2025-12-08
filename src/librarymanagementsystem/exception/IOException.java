e0fc579da7582cafa470097c1f64503b1410b562
Alivk <alirezavalikhani1998@gmail.com>
2025-12-08 10:46:54 +0330

    feat: Add some custom exceptions to project.

---
+++ src/librarymanagementsystem/exception/IOException.java
@@ -0,0 +1,7 @@
+package librarymanagementsystem.exception;
+
+public class IOException extends RuntimeException {
+    public IOException(String message) {
+        super(message);
+    }
+}
---
+++ src/librarymanagementsystem/exception/LogWriteException.java
@@ -0,0 +1,7 @@
+package librarymanagementsystem.exception;
+
+public class LogWriteException extends RuntimeException {
+    public LogWriteException(String message) {
+        super(message);
+    }
+}
